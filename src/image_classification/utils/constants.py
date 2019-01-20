7f69014ced04d0ac8220e1248ef71b4b29ce36b0
nicolagheza <ghzncl@gmail.com>
2019-01-20 22:42:39 +0100

    Update Inception v3 Transfer Learning Notebook

--- src/image_classification/utils/constants.py
+++ src/image_classification/utils/constants.py
@@ -6,8 +6,8 @@ fruit_models_dir = root_dir + 'fruit_models/'
 labels_file = root_dir + 'utils/labels'
 
 # change this to the path of the folders that hold the images
-training_images_dir = '/home/jeeza/Code/Fruit-Images-Dataset/Training'
-test_images_dir = '/home/jeeza/Code/Fruit-Images-Dataset/Test'
+training_images_dir = '/home/jeeza/Code/DeepFruit/Training'
+test_images_dir = '/home/jeeza/Code/DeepFruit/Test'
 
 # number of classes: number of fruit classes + 1 resulted due to the build_image_data.py script that leaves the first class as a background class
 # using the labels file that is also used in the build_image_data.py
