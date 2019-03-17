b814ec9436a7594a0a51549f790debeb836c4d07
Nicola Gheza <ghzncl@gmail.com>
2019-03-17 21:15:24 +0100

    Fixing for multiple training at once

--- src/cnn/network.py
+++ src/cnn/network.py
@@ -1,32 +1,32 @@
 from keras.models import Sequential
 from keras.layers import Dense, Activation, Conv2D, MaxPooling2D, BatchNormalization, Dropout, Flatten
 
-def cnn(input_shape, n_classes):
+def cnn(input_shape, cnn_filters, fc_units, n_classes):
     model = Sequential()
-    model.add(Conv2D(16, kernel_size=(5, 5), activation='relu', input_shape=input_shape))
+    model.add(Conv2D(cnn_filters[0], kernel_size=(5, 5), activation='relu', input_shape=input_shape))
     model.add(BatchNormalization())
 
-    model.add(Conv2D(32, kernel_size=(5, 5), activation='relu'))
+    model.add(Conv2D(cnn_filters[1], kernel_size=(5, 5), activation='relu'))
     model.add(BatchNormalization())
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(0.25))
 
-    model.add(Conv2D(64, kernel_size=(5, 5), activation='relu'))
+    model.add(Conv2D(cnn_filters[2], kernel_size=(5, 5), activation='relu'))
     model.add(BatchNormalization())
     model.add(Dropout(0.25))
 
-    model.add(Conv2D(128, kernel_size=(5, 5), activation='relu'))
+    model.add(Conv2D(cnn_filters[3], kernel_size=(5, 5), activation='relu'))
     model.add(BatchNormalization())
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(0.25))
 
     model.add(Flatten())
 
-    model.add(Dense(512, activation='relu')) # With [1024] memory is not enough
+    model.add(Dense(fc_units[0], activation='relu')) 
     model.add(BatchNormalization())
     model.add(Dropout(0.5))
 
-    model.add(Dense(256, activation='relu'))
+    model.add(Dense(fc_units[1], activation='relu'))
     model.add(BatchNormalization())
     model.add(Dropout(0.5))
 
@@ -36,4 +36,4 @@ def cnn(input_shape, n_classes):
               optimizer='Adam',
               metrics=['accuracy'])
 
-    return model
+    return model    
--- src/cnn/train.py
+++ src/cnn/train.py
@@ -4,11 +4,6 @@ from tensorflow.keras.callbacks import TensorBoard
 
 import pickle
 
-# Load training data
-NAME = "Fruit_Database-CNN-128"
-
-no_of_classes = 15
-
 pickle_in = open("C:/Users/nicol/Code/DeepFruit/Fruit_Database/X.pickle","rb")
 X = pickle.load(pickle_in)
 
@@ -16,14 +11,48 @@ pickle_in = open("C:/Users/nicol/Code/DeepFruit/Fruit_Database/y.pickle","rb")
 y = pickle.load(pickle_in)
 y = np_utils.to_categorical(y, no_of_classes) 
 
+no_of_classes = 15
+
 input_shape = (100, 100, 1)
 
-# Setup TensorBoard for Logs
-tensorboard = TensorBoard(log_dir='logs/{}'.format(NAME))
+cnn_filters = [
+    [16, 32, 64, 128], [8, 32, 64, 128], [32, 32, 64, 128], 
+    [16, 16, 64, 128], [16, 64, 64, 128], [16, 32, 32, 128], 
+    [16, 32, 128, 128], [16, 32, 64, 64], [16, 32, 64, 128], [16, 32, 64, 128]
+]
+fc_units = [[1024, 256], [512, 256], [1024, 512]]
 
-# Create Model and Start training
-model = cnn(input_shape, no_of_classes)
+def print_config(cnn_filters, fc_units):
+    for cnn_filter in cnn_filters:
+        print("Convolutional   | 5x5 | {}".format(cnn_filter))
+    for fc_unit in fc_units:
+        print("Fully connected |  -  | {}".format(fc_unit))
 
-model.summary()
+for i, cnn_filter in enumerate(cnn_filters):
+    if i < 8:
+        NAME = "Fruit_Database-CNN-{}x{}x{}x{}-{}x{}".format(
+        cnn_filter[0], cnn_filter[1], cnn_filter[2], cnn_filter[3],
+        fc_units[0][0], fc_units[0][1])
+        print("Current CNN config: {}".format(NAME))
+        print_config(cnn_filter, fc_units[0])
+        tensorboard = TensorBoard(log_dir='logs/{}'.format(NAME))
+        model = cnn(input_shape, cnn_filters[0], fc_units[0], no_of_classes)
+    elif i == 8:
+        NAME = "Fruit_Database-CNN-{}x{}x{}x{}-{}x{}".format(
+        cnn_filter[0], cnn_filter[1], cnn_filter[2], cnn_filter[3],
+        fc_units[1][0], fc_units[1][1])
+        print("Current CNN config: {}".format(NAME))
+        print_config(cnn_filter, fc_units[1])
+        tensorboard = TensorBoard(log_dir='logs/{}'.format(NAME))
+        model = cnn(input_shape, cnn_filters[0], fc_units[0], no_of_classes)
+    else:
+        NAME = "Fruit_Database-CNN-{}x{}x{}x{}-{}x{}".format(
+        cnn_filter[0], cnn_filter[1], cnn_filter[2], cnn_filter[3],
+        fc_units[2][0], fc_units[2][1])
+        print("Current CNN config: {}".format(NAME))
+        print_config(cnn_filter, fc_units[2])
+        tensorboard = TensorBoard(log_dir='logs/{}'.format(NAME))
+        model = cnn(input_shape, cnn_filters[0], fc_units[0], no_of_classes)
 
-history = model.fit(X, y, batch_size=32, epochs=100, validation_split=0.3, callbacks=[tensorboard])
+    history = model.fit(X, y, batch_size=32, epochs=1, validation_split=0.3, callbacks=[tensorboard])
+
