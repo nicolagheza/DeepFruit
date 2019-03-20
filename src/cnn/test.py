bf490c321037f01045ded6d08bcb0f8f6632cc4c
nicolagheza <ghzncl@gmail.com>
2019-03-20 22:36:47 +0000

    fix

--- src/cnn/test.py
+++ src/cnn/test.py
@@ -9,20 +9,22 @@ FLAGS = tf.app.flags.FLAGS
 
 no_of_classes = 15
 
-pickle_in = open("C:/Users/nicol/Code/DeepFruit/Fruit_Database/X.pickle","rb")
+pickle_in = open("/home/nicol/DeepFruit/X.pickle","rb")
 X = pickle.load(pickle_in)
 
-pickle_in = open("C:/Users/nicol/Code/DeepFruit/Fruit_Database/y.pickle","rb")
+pickle_in = open("/home/nicol/DeepFruit/y.pickle","rb")
 y = pickle.load(pickle_in)
 y = np_utils.to_categorical(y, no_of_classes) 
 
 input_shape = (100, 100, 1)
 
-model = cnn(input_shape, no_of_classes)
+model = cnn(input_shape, [16, 32, 64, 128], [1024, 256], no_of_classes)
 model.load_weights(FLAGS.model_path)
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=seed)
 
-scores = model.evaluate(X_test, y_test, verbose=1)
+loss, acc = model.evaluate(X_train, y_train)
+val_loss, val_acc = model.evaluate(X_test, y_test)
 
-print("Scores: ", scores)
+print("Accuracy: {}, Loss: {}".format(acc, los))
+print("Validation Accuracy: {}, Validation Loss: {}".format(val_acc, val_loss))
