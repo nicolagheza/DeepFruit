ddc853d5e8fcc45e36a6d8a387b413f420a53d73
Nicola Gheza <ghzncl@gmail.com>
2019-05-14 21:43:54 +0200

    minor changes to constants.py

--- src/image_classification/utils/constants.py
+++ src/image_classification/utils/constants.py
@@ -1,13 +1,13 @@
 import os
 
 root_dir = os.getcwd()
-data_dir = root_dir + '\\data\\'
-fruit_models_dir = root_dir + '\\fruit_models\\'
-labels_file = root_dir + '\\utils\\labels'
+data_dir = root_dir + '/data/'
+fruit_models_dir = root_dir + '/fruit_models/'
+labels_file = root_dir + '/utils/labels'
 
 # change this to the path of the folders that hold the images
-training_images_dir = 'C:\\Code\\Fruit360\\Training'
-test_images_dir = 'C:\\Code\\Fruit360\\Test'
+training_images_dir = 'C:/Code/Fruit360/Training'
+test_images_dir = 'C:/Code/Fruit360/Test'
 
 # number of classes: number of fruit classes + 1 resulted due to the build_image_data.py script that leaves the first class as a background class
 # using the labels file that is also used in the build_image_data.py
